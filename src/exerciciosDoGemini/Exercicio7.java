049800f29414b30f99134063ebe22f78d02da219
Leonardo <leonardo.pedrobr@gmail.com>
2025-12-22 10:53:14 -0300

    3.0

--- src/exerciciosDoGemini/Exercicio7.java
+++ src/exerciciosDoGemini/Exercicio7.java
@@ -1,4 +1,17 @@
 package exerciciosDoGemini;
+import java.util.Scanner;
 
 public class Exercicio7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha uma quantidade inteira de minutos: ");
+        int minutosUsuario = scanner.nextInt();
+
+        int horasInteiros = minutosUsuario/60;
+
+        int horasResto = minutosUsuario%60;
+
+        System.out.println("Esses minutos equivalem a "+horasInteiros+" horas e "+horasResto+" minutos.");
+    }
 }
