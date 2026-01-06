367b0eb21cf19a6197d8221375e6cf0f3625a646
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-05 22:20:53 -0300

    05.01.26.10.20

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -1,4 +1,30 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais03 {
+    public static void main (String[] args){
+        // doar se salario for maior que 5000
+        double salario = 4999;
+        String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        String mensagemNaoDoar = "Ainda nao tenho condicoes mas vou ter";
+        String resultado;
+
+        if(salario > 5000) {
+            resultado = mensagemDoar;
+        }else {
+            resultado = mensagemNaoDoar;
+        }
+
+        System.out.println(resultado);
+
+        //ISSO PODE SER FEITO UTILIZANDO-SE OPERADOR TERNARIO
+
+        double salario1 = 5000;
+        String mensagemDoar1 = "Eu vou doar 500 pro DeDojo1";
+        String mensagemNaoDoar1 = "Ainda nao tenho condicoes mas vou ter";
+
+        // (condicao) ? verdadeiro : falso
+        String resultado1 = salario1 > 5000 ? mensagemDoar1 : mensagemNaoDoar1;
+
+        System.out.println(resultado1);
+    }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+/*
+    I want to know how much i have to pay in the netherlands 2020 based on my annual salary
+
+    From (per year)               up to and including                   Tax
+    0                               34.712                              9.7%
+    34.713                          68.507                              37.35%
+    68.508                                                              49.50%
+*/
+
+public class Aula05EstruturasCondicionais04 {
+    
+}
