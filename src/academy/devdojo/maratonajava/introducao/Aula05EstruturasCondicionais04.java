2d2b36cd8f98f20dd4c22f65bad259dc66100bac
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-06 14:24:50 -0300

    06.01.20.26.14.24

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -1,4 +1,5 @@
 package academy.devdojo.maratonajava.introducao;
+import java.util.Scanner;
 
 /*
     I want to know how much i have to pay in the netherlands 2020 based on my annual salary
@@ -10,5 +11,16 @@ package academy.devdojo.maratonajava.introducao;
 */
 
 public class Aula05EstruturasCondicionais04 {
-    
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual é o valor do seu salário anual: ")
+        double valorSalarioAnual = scanner.nextLine();
+
+        double valorTaxaSalarioAnualPequeno = valorSalarioAnual*0
+
+        if(valorSalarioAnual >= 34712) {
+            
+        }
+    }
 }
