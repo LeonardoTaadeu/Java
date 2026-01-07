0cea092853ff3e7f87f82d8c3165118287ae8372
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-07 14:02:59 -0300

    07.01.20.26.14.02

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição04.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+import java.util.Scanner;
+
+public class Aula06EstruturasDeRepetição04 {
+        //dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
+        // A condição é que o valor da parcela tem que ser maior ou igual a 1000
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual o valor total do carro: ");
+    }
+}
