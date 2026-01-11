cf7771df93322bebbc0583694c28e87cf5f6c3fa
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-11 14:38:13 -0300

    11.01.20.26.14.38

--- src/exerciciosDoGemini/Exercicio12.java
+++ src/exerciciosDoGemini/Exercicio12.java
@@ -1,15 +1,17 @@
 package exerciciosDoGemini;
 import java.util.Scanner;
 
-// deve receber cinco notas, armazena-las e ver qual a maior
+// Solicite 5 notas de alunos, as notas podem ser numeros quebrados,
+// em seguida calcule a media da turma, imprima a media e liste quais notas ficaram acima da media
+
 
 public class Exercicio12 {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double contadorNotas = 0;
+        int contadorNotas = 0;
 
-        double[] notasAlunos = new double[4];
+        double[] notasAlunos = new double[5];
 
         while (contadorNotas < 5) {
             System.out.println("Qual sua nota: ");
@@ -19,6 +21,14 @@ public class Exercicio12 {
             contadorNotas++;
         }
 
+        double somaNotas = 5; //Essa variavel vai ter o valor da soma do Array
 
+        for (int i = 0; i < 4; i++) {
+            somaNotas += notasAlunos[i];
+        }
+
+        double divisaoNotas = somaNotas/5;
+
+        System.out.println("A media da turma vale: "+divisaoNotas);
     }
 }
