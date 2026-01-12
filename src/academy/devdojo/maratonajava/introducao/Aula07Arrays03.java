762a5cf83537dc10e4373065ca59d207ab44f82f
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-12 13:49:29 -0300

    12.01.20.26.13.49

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -5,10 +5,13 @@ public class Aula07Arrays03 {
     public static void main(String[] args) {
         int[] numeros = new int[3];
         int[] numeros2 = {1,2,3,4,5};
+        int[] numeros3 = new int[]{1,2,3,4,5};
 
         for (int i = 0; i < numeros2.length; i++) {
-            System.out.println(numeros2[i]);
+            System.out.println(numeros3[i]);
         }
-
+        for (int i : numeros3) {
+            System.out.println(i);
+        }
     }
 }
