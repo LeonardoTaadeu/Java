afa4e746babf90325dadfa543ca14effbfb34f2f
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-20 17:39:46 -0300

    20.01.20.26.17.39

---
+++ src/academy/devdojo/maratonajava/sistemafuncionarios/dominio/funcionario.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.sistemafuncionarios.dominio;
+
+public class funcionario {
+    public String nome;
+    public float salario;
+    public String cargo;
+    public String setor;
+}
