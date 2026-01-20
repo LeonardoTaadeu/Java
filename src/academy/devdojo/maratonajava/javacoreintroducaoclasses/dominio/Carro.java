41ec9ad30a7e519055add88e84f0d85c37e84795
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-19 21:15:10 -0300

    19.01.20.26.21.15

--- src/academy/devdojo/maratonajava/javacoreintroducaoclasses/dominio/Carro.java
+++ src/academy/devdojo/maratonajava/javacoreintroducaoclasses/dominio/Carro.java
@@ -2,6 +2,6 @@ package academy.devdojo.maratonajava.javacoreintroducaoclasses.dominio;
 
 public class Carro {
     public String nome;
-    public String modelo;
+    public String estado;
     public int ano;
 }
--- src/academy/devdojo/maratonajava/javacoreintroducaoclasses/teste/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacoreintroducaoclasses/teste/CarroTest01.java
@@ -1,7 +1,22 @@
 package academy.devdojo.maratonajava.javacoreintroducaoclasses.teste;
 
-import academy.devdojo.maratonajava.javacoreintroducaoclasses.dominio.Professor;
+import academy.devdojo.maratonajava.javacoreintroducaoclasses.dominio.Carro;
 
 public class CarroTest01 {
-    
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Carro carro2 = new Carro();
+
+        carro.nome = "Ford";
+        carro.estado = "Novo";
+        carro.ano = 1990;
+        System.out.println("Nome do carro: "+carro.nome+", esta no estado "+carro.estado+", do ano de "+carro.ano);
+
+        carro2.nome = "Gurgel";
+        carro2.estado = "Velho";
+        carro2.ano = 1970;
+        System.out.println("Nome do carro: "+carro2.nome+", esta no estado "+carro2.estado+", do ano de "+carro2.ano);
+
+
+    }
 }
