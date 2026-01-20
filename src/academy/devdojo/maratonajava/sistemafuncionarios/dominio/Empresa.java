16fa308ceabbd6016fe3e4d74905394903fe5393
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-20 19:39:33 -0300

    20.01.20.26.19.39

--- src/academy/devdojo/maratonajava/sistemafuncionarios/dominio/Empresa.java
+++ src/academy/devdojo/maratonajava/sistemafuncionarios/dominio/Empresa.java
@@ -1,4 +1,9 @@
 package academy.devdojo.maratonajava.sistemafuncionarios.dominio;
 
 public class Empresa {
+    public static void main (String[] args) {
+        String[][] funcionariosEmpresa = new String[20][4]; //Vinte funcionarios, cada um com 4 especificacoes
+
+
+    }
 }
