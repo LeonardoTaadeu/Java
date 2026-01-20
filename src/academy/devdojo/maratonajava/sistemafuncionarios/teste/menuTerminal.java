3128e40f449659752491e1e20a92649d23f322ee
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-20 20:43:52 -0300

    20.01.20.26.20.43

--- src/academy/devdojo/maratonajava/sistemafuncionarios/teste/menuTerminal.java
+++ src/academy/devdojo/maratonajava/sistemafuncionarios/teste/menuTerminal.java
@@ -1,45 +1,49 @@
-    package academy.devdojo.maratonajava.sistemafuncionarios.teste;
+package academy.devdojo.maratonajava.sistemafuncionarios.teste;
 
-    import java.util.Scanner;
+import java.util.Scanner;
 
-    //Criando um sistema onde o usuario vai cadastrar funcionarios, ver a media de salario e consegue acessar os menus atraves dos numerais
+//Criando um sistema onde o usuario vai cadastrar funcionarios, ver a media de salario e consegue acessar os menus atraves dos numerais
 
-    public class menuTerminal {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+public class menuTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("|----------------------------|");
-            System.out.println("| Opção 1: Novo Funcionario  |");
-            System.out.println("| Opção 2: Funcionarios      |");
-            System.out.println("| Opção 3: Estatisticas      |");
-            System.out.println("| Opção 4: Encerrar          |");
-            System.out.println("|----------------------------|");
-            System.out.println("-------Digite uma Opção ------");
+        System.out.println("|----------------------------|");
+        System.out.println("| Opção 1: Novo Funcionario  |");
+        System.out.println("| Opção 2: Funcionarios      |");
+        System.out.println("| Opção 3: Estatisticas      |");
+        System.out.println("| Opção 4: Encerrar          |");
+        System.out.println("|----------------------------|");
+        System.out.println("-------Digite uma Opção ------");
 
-            int opcaoMenu = scanner.nextInt();
-        }
+        int opcaoMenu = scanner.nextInt();
+    }
 
-        public static void novoFuncionario {
-            Scanner scanner = new Scanner(System.in);
+    public static void novoFuncionario() {
+        Scanner scanner = new Scanner(System.in);
 
-            int confirmarNovoFuncionario = 1;
+        int confirmarNovoFuncionario = 1;
 
-            while(confirmarNovoFuncionario == 1) { // and array < 20)
-                System.out.println("Nome: ");
-                String nomeFuncionario = scanner.nextLine();
+        while (confirmarNovoFuncionario == 1) { // and array < 20)
+            System.out.println("Nome: ");
+            String nomeFuncionario = scanner.nextLine();
 
-                System.out.println("Cargo: ");
-                String nomeCargo = scanner.nextLine();
+            System.out.println("Cargo: ");
+            String nomeCargo = scanner.nextLine();
 
-                System.out.println("Setor: ");
-                String setorFuncionario = scanner.nextLine();
+            System.out.println("Setor: ");
+            String setorFuncionario = scanner.nextLine();
 
-                System.out.println("Salario: ");
-                float salarioFuncionario = scanner.nextFloat();
+            System.out.println("Salario: ");
+            float salarioFuncionario = scanner.nextFloat();
 
-                System.out.println("Deseja inserir um novo usuario? Caso sim digite 1, caso nao digite 0: ");
-                confirmarNovoFuncionario = scanner.nextInt();
-                scanner.nextLine();
-            }
+            System.out.println("Deseja inserir um novo usuario? Caso sim digite 1, caso nao digite 0: ");
+            confirmarNovoFuncionario = scanner.nextInt();
+            scanner.nextLine();
         }
     }
+
+    public static void listarFuncionarios() {
+        
+    }
+}
