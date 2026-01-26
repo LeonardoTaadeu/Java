614e521cb3abe9a935b2aa3b55497e6c82ecec81
Leonardo <leonardo.pedrobr@gmail.com>
2026-01-26 13:57:04 -0300

    26.01.20.26.13.57

--- src/academy/devdojo/maratonajava/sistemafuncionarios/dominio/Empresa.java
+++ src/academy/devdojo/maratonajava/sistemafuncionarios/dominio/Empresa.java
@@ -1,7 +1,19 @@
 package academy.devdojo.maratonajava.sistemafuncionarios.dominio;
 
 public class Empresa {
-    private String[][] funcionariosEmpresa = new String[20][4];
+    private Funcionario[] funcionarios = new Funcionario[20];
+    private int index = 0;
 
+    public void adicionarFuncionario(Funcionario funcionario) {
+        if (index < funcionario.lenght) {
+            funcionarios[index++] = funcionario;
+        } else {
+            System.out.println("Limite de funcionários atingido");
+        }
+    }
+
+    public Funcionario[] getFuncionarios {
+        return funcionarios;
+    }
 }
     
