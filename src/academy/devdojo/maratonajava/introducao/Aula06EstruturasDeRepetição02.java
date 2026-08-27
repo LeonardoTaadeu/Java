cdbca8d3a80fd7175f22148f24c7ce718a47dd87
Leonardo Tadeu <leonardo.tadeu@ofdc.local>
2026-08-27 10:44:31 -0300

    Refazendo o exercicio do 1 milhão

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição02.java
@@ -5,10 +5,8 @@ public class Aula06EstruturasDeRepetição02 {
     //Imprima todos os números pares de 0 até 1000000
 
     public static void main(String[] args) {
-        for(int i=0; i <= 100; i++) {
-            if(i % 2 == 0) {
-                System.out.println(i);
-            }
+        for(int i=0; i <= 1000000; i++){
+            System.out.println(i);
         }
     }
 }
