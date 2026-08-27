e026dabb49c6298ff94d996d6553bde921b9213d
Leonardo Tadeu <leonardo.tadeu@ofdc.local>
2026-08-27 10:57:58 -0300

    colocando contagem até 1 milhão somente com números pares

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição02.java
@@ -6,7 +6,9 @@ public class Aula06EstruturasDeRepetição02 {
 
     public static void main(String[] args) {
         for(int i=0; i <= 1000000; i++){
-            System.out.println(i);
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
