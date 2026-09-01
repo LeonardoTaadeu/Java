5b0eb6071c3fdd5c10a2289047956eef3eccda28
Leonardo Tadeu <leonardo.pedrobr@gmail.com>
2026-08-31 23:27:01 -0300

    Opa

---
+++ src/academy/devdojo/maratonajava/javacoreintroducaoclasses/TesteAula/PessoaTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacoreintroducaoclasses.TesteAula;
+
+import academy.devdojo.maratonajava.javacoreintroducaoclasses.dominioAula.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa estudante = new Pessoa();
+    }
+}
