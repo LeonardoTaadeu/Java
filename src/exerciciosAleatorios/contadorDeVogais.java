0f7de0034f8476da7f8068e580570394a8e8ae6b
Leonardo Tadeu <leonardo.pedrobr@gmail.com>
2026-09-04 22:49:46 -0300

    nao terminei ainda mas vou dormir

---
+++ src/exerciciosAleatorios/contadorDeVogais.java
@@ -0,0 +1,16 @@
+package exerciciosAleatorios;
+
+import java.util.Scanner;
+
+public class contadorDeVogais {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite uma palavra: ");
+        String palavra = scanner.nextLine();
+
+        int contadorLetra = 0;
+
+        for(int i; )
+    }
+}
