5cc5ec400f1c7ef78b7f5ad45703187c51fccec6
Leonardo Tadeu <leonardo.pedrobr@gmail.com>
2026-09-06 20:55:39 -0300

    opa

--- src/exerciciosAleatorios/contadorDeVogais.java
+++ src/exerciciosAleatorios/contadorDeVogais.java
@@ -1,16 +1,29 @@
-package exerciciosAleatorios;
-
 import java.util.Scanner;
 
 public class contadorDeVogais {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite uma palavra: ");
-        String palavra = scanner.nextLine();
+        System.out.print("Digite uma palavra: ");
+        String palavra = scanner.nextLine().toLowerCase();
 
-        int contadorLetra = 0;
+        int contador = 0;
 
-        for(int i; )
+        for (int i = 0; i < palavra.length(); i++) {
+
+            char caractere = palavra.charAt(i);
+
+            if (caractere == 'a' || caractere == 'e' ||
+                    caractere == 'i' || caractere == 'o' ||
+                    caractere == 'u') {
+
+                contador++;
+            }
+        }
+
+        System.out.println("Quantidade de vogais: " + contador);
+
+        scanner.close();
     }
 }
